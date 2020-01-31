2ea1bb605e08bec7fb8167171cb6a10eee801f2f
Anna Jones <sc19akj@leeds.ac.uk>
2020-01-31 22:01:26 +0000

    Test Commit - Input Handler Added

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public static int X_direction;
+    public static int Y_direction;
+    public static bool JumpFlag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        X_direction = 0;
+        Y_direction = 0;
+        JumpFlag = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        JumpFlag = Input.GetButtonDown("Jump");
+        X_direction = (int)Input.GetAxis("Horizontal");
+    }
+}
