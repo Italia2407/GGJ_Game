c5e3c352bfc6961d5d9bc34d7fe6b1386df45e05
Anna Jones <sc19akj@leeds.ac.uk>
2020-02-01 10:21:10 +0000

    Added Movement Script, but it is not finished

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -1,13 +1,32 @@
-﻿using System.Collections;
+﻿/*
+    InputHandler.cs
+    This script is will update every frame, and detect the user's input.
+    This script is accessable across all modules. Access the script using InputHander._________ if you want to access something
+    Is only intended to process input and nothing else
+    Will ONLY be attached to the MainHandler Object
+*/
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class InputHandler : MonoBehaviour
 {
+    /*
+    Variables:
+    X_direction = will detect which direction the player is facing
+    Y_direction = will detect whether the player is going up or down. Note: May end up being redundant
+    JumpFlag = will detect if the user has pressed the jump button
+    Interact Flag = will detect if the player is pressed the interact button (left shift)
+    */
+
+
     public static int X_direction;
     public static int Y_direction;
-    public static bool JumpFlag;
-    // Start is called before the first frame update
+    public static bool JumpFlag, InteractFlag;
+
+    // Start is called before the first frame update. Called at the beginning of the program to declare variables
     void Start()
     {
 
@@ -17,12 +36,26 @@ public class InputHandler : MonoBehaviour
         Y_direction = 0;
         //is the player jumping?
         JumpFlag = false;
+        //is the player interacting with an object?
+        InteractFlag = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*
+        - Get which buttons the player is pressing
+        */
         JumpFlag = Input.GetButtonDown("Jump");
-        X_direction = (int)Input.GetAxis("Horizontal");
+        InteractFlag = Input.GetButtonDown("Fire3");
+        if (Input.GetAxis("Horizontal") > 0)
+        { X_direction = 1; }
+        else if (Input.GetAxis("Horizontal") < 0)
+        { X_direction = -1; }
+        else
+        {
+            X_direction = 0;
+        }
+        
     }
 }
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,62 @@
+﻿/*
+Movement.cs
+This will move the player depending on the input of the script InputHandler.cs
+This Script is accessible across all scripts using Movement._________
+Must only be declared on Main Handler object
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    /*
+    Variables:
+    instance - itself (do not touch this)
+    player - for the player's movement
+    rateofMovement - the speed of the player
+
+    */
+    public static Movement instance;
+    public static Rigidbody2D player;
+    public static float RateOfMovement;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        //rate of movement is declared here
+        RateOfMovement = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if the player isn't found
+        if (player == null)
+        {
+            //find the object the player's rigid body is attached to
+            GameObject playerbaseclass = GameObject.Find("Test Player");
+            //if found
+            if (playerbaseclass != null)
+            {
+                //declare player
+                player = playerbaseclass.GetComponent<Rigidbody2D>();
+
+            }
+            else
+            {
+                Debug.LogErrorFormat("Player has not been found);
+            }
+        }
+        //now if player is found
+        if (player != null)
+        {
+            //move player horizontally depending on the input
+            player.velocity = new Vector2(InputHandler.X_direction * RateOfMovement, player.velocity.y); 
+        }
+
+    }
+}
