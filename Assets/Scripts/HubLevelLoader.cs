aca78b0c2e20086cfb0a47cdc1e5970535880c4c
Ismaele Giuseppe Recca <igrecca27@outlook.com>
2020-02-02 13:08:35 +0000

    Added level transition functionality to hub.

---
+++ Assets/Scripts/HubLevelLoader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HubLevelLoader : InteractableBase
+{
+    public string sceneName;
+
+    public override void Interact()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
